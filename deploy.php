0688e6967371ee960f636f87f4e440fb4a88323b
andrew farquharson <farquharson.a@gmail.com>
2022-06-24 22:17:23 +0100

    change config sets

--- deploy.php
+++ deploy.php
@@ -10,6 +10,8 @@ require 'contrib/rsync.php';
 set('application', 'yamaha-avantgrand');
 set('repository', 'git@github.com:cactus-blossom-it-services-limited/yamaha-avantgrand.git');
 set('ssh_multiplexing', true);  // Speed up deployment
+set('identity_file', '~/.ssh/id_rsa');
+set('deploy_path', '/var/www/cactusblossomitservices.com');
 //set('default_timeout', 1000);
 
 set('rsync_src', function () {
