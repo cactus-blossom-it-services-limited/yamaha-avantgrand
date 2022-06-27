962324f2e420a39244e8fa524ce18c42147a5723
andrew farquharson <farquharson.a@gmail.com>
2022-06-27 10:12:51 +0100

    fixes for phpunit testing

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-// Route::post('/books', 'App\Http\Controllers\BooksController@store');
+Route::post('/books', 'App\Http\Controllers\BooksController@store');
